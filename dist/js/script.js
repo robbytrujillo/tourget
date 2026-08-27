99984143e9bf999329893ca4b25b9ed03958dd0c
robbytrujillo <robbyikusuma88@gmail.com>
2026-08-28 05:32:59 +0700

    introduction Tourget

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,3 +5,15 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
   menu.classList.toggle("menu-active");
 });
+
+const loader = document.querySelector(".loader");
+
+function loaderActive() {
+  loader.classList.add("loader-active");
+}
+
+function loaderActiveTime() {
+  setInterval(loaderActive, 3000);
+}
+
+window.onload = loaderActiveTime();
