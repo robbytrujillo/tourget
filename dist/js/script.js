2acd6a21dc43ff52257224ee2a1c374493f6b7f1
robbytrujillo <robbyikusuma88@gmail.com>
2026-09-01 14:00:28 +0700

    introduction Tourget

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
+// memunculkan hamburger menu
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
   menu.classList.toggle("menu-active");
 });
 
+// menghilangkan hamburger menu
 hamburger.addEventListener("scroll", () => {
   hamburger.classList.remove("is-active");
   menu.classList.remove("menu-active");
